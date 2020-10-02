075ceab9ea8c8170663d9ba33aed5bfd904b91b9
Juliano Seus <juliano.seus@hotmail.com>
2020-10-02 15:22:30 -0300

    Funcionamento Geral Basico - OK

--- js/chat.js
+++ js/chat.js
@@ -1,26 +1,83 @@
 window.onload = function() {
-    function createChatBox() {
+    
+    function twodigits(time) {
+        if (time < 10) {
+            return "0" + time
+        }
+        else {
+            return time;
+        }
+    }
+    
+    function getTime() {
+        var d = new Date();
+        var time;
+        var hour = d.getHours();
+        var minute = d.getMinutes();
+
+        time = twodigits(hour) + ":" + twodigits(minute);
+        return time;
+        
+    }
+    
+    function createChatBox(message = "a", side = 1) {
         
         var chatbox = document.createElement("div");
+        var messagetemplate = document.createElement("div");
         var chaticon = document.createElement("img");
         var messageFather = document.createElement("p");
-        var messageChild = document.createTextNode("teste");
+        var messageChild = document.createTextNode(message);
         var dateFather = document.createElement("p");
-        var dateChild = document.createTextNode("16:00");
+        var dateChild = document.createTextNode(getTime());
         
-        chatbox.className = "message-template";
+        chatbox.className = "message-box";
+        
+        if (side == 1)
+            {
+            messagetemplate.className = "message-template left";
+            }
+        else
+            {
+             messagetemplate.className = "message-template right";   
+            }
         
         chaticon.src = "img/icon-person.png";
         
         messageFather.appendChild(messageChild);
         dateFather.appendChild(dateChild);
         
-        chatbox.appendChild(chaticon);
-        chatbox.appendChild(messageFather);
-        chatbox.appendChild(dateFather);
+        messagetemplate.appendChild(chaticon);
+        messagetemplate.appendChild(messageFather);
+        messagetemplate.appendChild(dateFather);
+        
+        chatbox.appendChild(messagetemplate);
         
         document.getElementById("chat").appendChild(chatbox);
     }
     
-    createChatBox();
+    document.getElementById("inputleft").onkeypress = function() {
+        
+        var keypressed = event.keyCode;
+        
+        if (keypressed == 13)
+            {
+                var text = document.getElementById("inputleft").value;
+                createChatBox(text, 1);
+                document.getElementById("inputleft").value = "";
+            }
+    }
+    
+    document.getElementById("inputright").onkeypress = function() {
+        
+        var keypressed = event.keyCode;
+        
+        if (keypressed == 13)
+            {
+                var text = document.getElementById("inputright").value;
+                createChatBox(text, 2);
+                document.getElementById("inputright").value = "";
+            }
+    }
+    
+    
 }
