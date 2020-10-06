8e4587ac3ccbf08a7529573b2223d2c4349cf422
Juliano Seus <juliano.seus@hotmail.com>
2020-10-06 16:40:49 -0300

    Todas as funcionalidades básicas foram implementadas

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     
+    /*FUNÇÔES*/
     function twodigits(time) {
         if (time < 10) {
             return "0" + time
@@ -20,49 +21,52 @@ window.onload = function() {
         
     }
     
-    function createChatBox(message = "a", side = 1) {
+    function createChatBox(message = "a", side = 1, name = "Name") {
         
         var chatbox = document.createElement("div");
         var messagetemplate = document.createElement("div");
         var chaticon = document.createElement("img");
+        var nameFather = document.createElement("p");
+        var nameChild = document.createTextNode(name);
         var messageFather = document.createElement("p");
         var messageChild;
-        if (side == 1)
-            {
-                messageChild = document.createTextNode("Lorem: " + message);
-            }
-        else 
-            {
-                messageChild = document.createTextNode("Ipsum: " + message);
-            }
+        var dateFather = document.createElement("span");       
+        var dateChild = document.createTextNode(getTime());      
         
-        var dateFather = document.createElement("p");
-        
-        var dateChild = document.createTextNode(getTime());
-        
-        chatbox.className = "message-box";
         
         if (side == 1)
             {
-            messagetemplate.className = "message-template left";
+                chaticon.src = "img/icon-person1.png";
+                messagetemplate.className = "message-template left";
+                document.getElementById("inputleft").value = "";
+                document.getElementById("animation-left").style.display = 'none';
             }
         else
             {
-             messagetemplate.className = "message-template right";   
+                chaticon.src = "img/icon-person2.png";
+                messagetemplate.className = "message-template right";   
+                document.getElementById("inputright").value = "";
+                document.getElementById("animation-right").style.display = 'none';
             }
         
-        chaticon.src = "img/icon-person.png";
+        messageChild = document.createTextNode(message);  
+        
+        chatbox.className = "message-box";
+        
+        nameFather.appendChild(nameChild);
         
         messageFather.appendChild(messageChild);
         dateFather.appendChild(dateChild);
         
         messagetemplate.appendChild(chaticon);
+        messagetemplate.appendChild(nameFather);
         messagetemplate.appendChild(messageFather);
         messagetemplate.appendChild(dateFather);
         
         chatbox.appendChild(messagetemplate);
         
         document.getElementById("chat").appendChild(chatbox);
+        updateScroll();
     }
     
     function updateScroll() {
@@ -70,64 +74,104 @@ window.onload = function() {
         elem.scrollTop = elem.scrollHeight;
     }
     
-    document.getElementById("inputleft").onkeypress = function() {
+    /*/////////////////////////////////////////////////////////////////////////////
+    *//////////////////////////////////////////////////////////////////////////////
+    /*/////////////////////////////////////////////////////////////////////////////
+    *//////////////////////////////////////////////////////////////////////////////
+    
+    /*CONFIGURAÇÕES INICIAIS DA TELA*/
+    
+    createChatBox("Entrou no chat", 1, "Ipsum");
+    createChatBox("Entrou no chat", 2, "Lorem");
+    document.getElementById("animation-left").style.display = 'none';
+    document.getElementById("animation-right").style.display = 'none';
+    
+    /*/////////////////////////////////////////////////////////////////////////////
+    *//////////////////////////////////////////////////////////////////////////////
+    /*/////////////////////////////////////////////////////////////////////////////
+    *//////////////////////////////////////////////////////////////////////////////
+
+     
+
+    /*CONFIGURAÇÔES DOS ELEMENTOS*/
+    document.getElementById("inputleft").onkeydown= function() {
+        
+        const Enter = 13;
+        const BackSpace = 8;
         
         var keypressed = event.keyCode;
         var text = document.getElementById("inputleft").value;
         
-        if (text != "")
+        if (text.trim().length > 0)
         { 
-            if (keypressed == 13)
+            if (keypressed == Enter)
                 {
-                    
-                    createChatBox(text, 1);
-                    document.getElementById("inputleft").value = "";
-                    updateScroll();
+                    createChatBox(text, 1, "Ipsum");
                 }
             else
                 {
-                    var count = document.getElementById("console-chat").children().length;
-                    alert(count);
+                    document.getElementById("animation-left").style.display = 'block';
                 }
         }
+        else
+        {
+            document.getElementById("animation-left").style.display = 'none';
+        }
+        
+        if (keypressed == BackSpace && text.trim().length < 2)
+            {
+                document.getElementById("animation-left").style.display = 'none';
+            }
     }
     
-    document.getElementById("inputright").onkeypress = function() {
+    document.getElementById("inputright").onkeydown = function() {
+        
+        const Enter = 13;
+        const BackSpace = 8;
         
         var keypressed = event.keyCode;
         var text = document.getElementById("inputright").value;
         
-        if (text != "")
+        if (text.trim().length > 0)
         { 
-            if (keypressed == 13)
+            if (keypressed == Enter)
+                {  
+                    createChatBox(text, 2, "Lorem");
+                }
+             else
                 {
-                    
-                    createChatBox(text, 2);
-                    document.getElementById("inputright").value = "";
-                    updateScroll();
+                    document.getElementById("animation-right").style.display = 'block';
                 }
         }
+        else
+        {
+            document.getElementById("animation-right").style.display = 'none';
+        }
+        
+        if (keypressed == BackSpace && text.trim().length < 2)
+            {
+                document.getElementById("animation-left").style.display = 'none';
+            }
     }
     
     document.getElementById("send-left").onclick = function () {
         
         var text = document.getElementById("inputleft").value;
-        if (text != "")
+        
+        if (text.trim().length > 0)
         {
             createChatBox(text, 1);
-            document.getElementById("inputleft").value = "";
-            updateScroll();
         }
     }
     
     document.getElementById("send-right").onclick = function () {
         
         var text = document.getElementById("inputright").value;
-        if (text != "")
+        
+        if (text.trim().length > 0)
         {
             createChatBox(text, 2);
-            document.getElementById("inputright").value = "";
-            updateScroll();
+
         }
     }   
     
